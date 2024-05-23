64e920ecc89c44de7285f5cb1f31fdaefc746c9b
Zeynep <98862518+Zkaya9@users.noreply.github.com>
2024-05-23 18:56:18 -0500

    Update HelloWorld.java

--- src/main/java/org/launchcode/HelloWorld.java
+++ src/main/java/org/launchcode/HelloWorld.java
@@ -4,10 +4,14 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println(sayHello());
+
+        
     }
 
     public static String sayHello() {
         return "Hello, World!";
+
+        
     }
 
 }
